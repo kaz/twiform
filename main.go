9b20099ad187f3b9968019915124b3472178aced
Kazuki Sawada <kazuki@6715.jp>
2020-03-07 21:31:51 +0900

    add ui

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/kaz/twiform/state"
 	"github.com/kaz/twiform/sync"
 )
 
 func main() {
-	syn, err := sync.New(state.NewJsonStore("state.json"))
+	if os.Args[1] == "serve" {
+		serve()
+	}
+
+	s, err := sync.New(state.NewJsonStore("state.json"))
 	if err != nil {
 		panic(err)
 	}
 
-	syn.Plan()
+	switch os.Args[1] {
+	case "plan":
+		s.Plan()
+	case "apply":
+		s.Apply()
+	default:
+		fmt.Println("no such subcommand: ", os.Args[1])
+	}
+}
+
+func serve() {
+	fmt.Println("serving at http://localhost:8080")
+	panic(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 }
